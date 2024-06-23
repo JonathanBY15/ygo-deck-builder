14a77912412d1095493d6b88fc95b2c53b7ca3df
JonathanBY15 <bariscanyaprak@gmail.com>
2024-06-22 23:53:31 -0400

    Added username and email validation to register form
    Added more validation to register form. Added Login form.

--- forms.py
+++ forms.py
@@ -14,3 +14,23 @@ class RegisterForm(FlaskForm):
         """Validate that password_confirm matches password."""
         if password_confirm.data != self.password.data:
             raise ValidationError('Passwords must match')
+        
+    def validate_username(self, username):
+        """Validate that username is unique."""
+        from models import User
+        if User.query.filter_by(username=username.data).first():
+            raise ValidationError('Username already taken')
+        
+    def validate_email(self, email):
+        """Validate that email is unique."""
+        from models import User
+        if User.query.filter_by(email=email.data).first():
+            raise ValidationError('Email already taken')
+        
+class LoginForm(FlaskForm):
+    """Form for logging in a user."""
+    
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    
